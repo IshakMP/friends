a06827d08767bd54abdb6adbbb977ffe3ff151d9
IshakMP <ishqmp@gmail.com>
2023-03-13 16:26:15 +0530

    Created Skeleton  - API Side

---
+++ APIz/Data/DataContext.cs
@@ -0,0 +1,16 @@
+
+
+using APIz.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIz.Data
+{
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+  }
+}
